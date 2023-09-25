baece94f7f1ead5f4d7958d0936e48ffe95a6f42
awdem <wdemontmollin@gmail.com>
2023-09-25 11:37:49 +0100

    basic board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </React.Fragment>
+  );
 }
