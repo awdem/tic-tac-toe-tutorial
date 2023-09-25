2248c947b8d2276505802c1b2b14b1bc23703f0e
awdem <wdemontmollin@gmail.com>
2023-09-25 11:51:40 +0100

    creates square component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-export default function Square() {
+const Square = () => {
+  return <button className="square">1</button>
+}
+
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <Square />
+      <Square />
+      <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <Square />
+      <Square />
+      <Square />
       </div>
     </>
   );
