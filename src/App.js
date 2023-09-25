644f56bb14f23123e270ddb143c89400592d1e64
awdem <wdemontmollin@gmail.com>
2023-09-25 15:48:03 +0100

    refactor: removes state tracking of xIsNext because it is always true when currentMove is even

--- src/App.js
+++ src/App.js
@@ -56,21 +56,19 @@ const Board = ({ xIsNext, squares, onPlay }) => {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove]; // sets current squares to the element of the history array at the current move
 
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]; // creates a new history array by appending the new squares in the current move's position
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1); // sets the current move to the index of the latest element of the new history array
-    setXIsNext(!xIsNext);
   }
 
   const jumpTo = (nextMove) => {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move)=> {
